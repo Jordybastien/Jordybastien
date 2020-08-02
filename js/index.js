d55bea43e0301da15e155888151ffe0e9b465f07
Jordybastien <rugumbirajordybastien@gmail.com>
2020-08-02 12:24:40 +0200

    feat: Portfolio addons

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 // Fix menu on scroll
-window.onscroll = () => {
-  let header = document.getElementsByTagName("nav");
-  window.pageYOffset > 400
-    ? header[0].classList.add("sticky-nav")
-    : header[0].classList.remove("sticky-nav");
-};
+// window.onscroll = () => {
+//   let header = document.getElementsByTagName('nav');
+//   window.pageYOffset > 400
+//     ? header[0].classList.add('sticky-nav')
+//     : header[0].classList.remove('sticky-nav');
+// };
 // scrollIntoView
-goToDiv = id => {
+goToDiv = (id) =>
   document
     .getElementById(id)
-    .scrollIntoView({ block: "center", behavior: "smooth" });
-};
+    .scrollIntoView({ block: 'start', behavior: 'smooth', inline: 'start' });
+
+// Footer Year
+window.onload = () =>
+  (document.getElementById('copyright').innerHTML = new Date().getFullYear());
